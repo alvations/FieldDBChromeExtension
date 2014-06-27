f8cf247e3654497d047dc99afd2ee49749ebc721
cesine <cesine@yahoo.com>
2014-06-26 22:50:12 -0400

    not using transliteration

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,36 +2,43 @@
 'use strict';
 
 
-var translitit = Translitit;
+// var translitit = Translitit;
 chrome.storage.sync.get({
-	useTransliterator: true
+    useTransliterator: true
 }, function(items) {
-	if (items.useTransliterator) {
-		window.setInterval(function(){
-			var walkTheDOM = function walk(node, func) {
-				func(node);
-				node = node.firstChild;
-				while (node) {
-					walk(node, func);
-					node = node.nextSibling;
-				}
-			};
-			walkTheDOM(document.body, function(node) {
-				if (node.nodeType === 1) {
-					// alert(node.id); // alert if we have a type 1 node
-					if (node.childElementCount === 0) {
-						var previousText = node.textContent ? node.textContent.trim() : '';
-						if (previousText) {
-							// console.log(previousText);
-							var changedText = translitit(previousText);
-							node.innerHTML = changedText;
-							node.title = previousText;
-						}
-					}
-				}
-			});
-		}, 2000);
-	} else {
-		console.log('Transliteration is off');
-	}
+    if (items.useTransliterator) {
+        window.datum = {
+            orthography: '',
+            transliteration: ''
+        };
+        window.setInterval(function() {
+
+            var walkTheDOM = function walk(node, func) {
+                func(node);
+                node = node.firstChild;
+                while (node) {
+                    walk(node, func);
+                    node = node.nextSibling;
+                }
+            };
+            walkTheDOM(document.body, function(node) {
+                if (node.nodeType === 1) {
+                    // alert(node.id); // alert if we have a type 1 node
+                    if (node.childElementCount === 0) {
+                        var previousText = node.textContent ? node.textContent.trim() : '';
+                        if (previousText) {
+                            // console.log(previousText);
+                            // var changedText = Tanslitit(previousText);
+                            window.datum.orthography += ' ' + previousText;
+                            // window.datum.transliteration += ' ' + changedText;
+                            // node.innerHTML = changedText;
+                            // node.title = changedText;
+                        }
+                    }
+                }
+            });
+        }, 2000);
+    } else {
+        console.log('Transliteration is off');
+    }
 });
